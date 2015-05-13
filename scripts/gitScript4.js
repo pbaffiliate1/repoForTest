6bb8428c7f964f5e1e43594201d2d59514230146
pbaffiliate1 <pbaffiliate1@gmail.com>
2015-05-13 11:40:05 +0300

    This is a test commit message

---
+++ scripts/gitScript4.js
@@ -0,0 +1,18 @@
+/** Script ACLs do not delete 
+ read=all 
+write=all
+execute=all 
+  **/ 
+ 
+	
+  
+/**
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+**/
+
+return "testing the commented out content1";
+
+
