2311e38cda1ad7492c70a37b484937b61a677747
pbaffiliate1 <pbaffiliate1@gmail.com>
2016-06-14 15:00:18 +0300

    This is a test commit message

--- scripts/gitMetadataScript1.js
+++ scripts/gitMetadataScript1.js
@@ -1,14 +1,6 @@
-<script>
-<scriptACL>
-<execute>all</execute>
-<read>all</read>
-<write>all</write>
-</scriptACL>
-<code> 
-<![CDATA[	
+ 
+	
          
 return "heavy metal will not die";
 
-]]>
-</code>
-</script>
+
