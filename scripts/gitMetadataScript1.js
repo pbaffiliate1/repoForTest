4bebbc4afff18ea1eec286e2e9c04e88a5b1227b
pbaffiliate1 <pbaffiliate1@gmail.com>
2016-06-16 14:26:54 +0300

    Commit from Test -  scripts/gitMetadataScript1.js

---
+++ scripts/gitMetadataScript1.js
@@ -0,0 +1,14 @@
+<script>
+<scriptACL>
+<execute>all</execute>
+<read>all</read>
+<write>nobody</write>
+</scriptACL>
+<code> 
+<![CDATA[	
+         
+return "heavy metal will not die";
+
+]]>
+</code>
+</script>
